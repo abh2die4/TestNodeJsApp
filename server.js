d1f2e7f6f4ec6ce1e367259b6c51ebb6993bd6cd
Debadutta <adebadutta@deloitte.com>
2016-03-22 23:18:56 +0530

    chnged time

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
