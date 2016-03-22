79aa53ed01ab51762d908b0883422dadd25f965f
Debadutta <adebadutta@deloitte.com>
2016-03-22 22:11:55 +0530

    new code piece

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-const http = require('http');
-
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
